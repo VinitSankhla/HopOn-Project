c6fc1ea25c951df17c3634700efef2e3dbf3ed20
Vinit Sankhla <vinitsankhla2006@gmail.com>
2025-09-23 00:27:46 +0530

    cors fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin : ["https://main.d2v2y6uuqi1nje.amplifyapp.com/"]
+}));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
