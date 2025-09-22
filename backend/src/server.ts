73f80d14712c7cb4f84578e33c6e4821cea7f2cd
Vinit Sankhla <vinitsankhla2006@gmail.com>
2025-09-23 02:14:54 +0530

    Test 5

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,12 +17,16 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin:['https://main.d2v2y6uuqi1nje.amplifyapp.com/']
+  }
+));
 
 
 
 app.use(express.json({ limit: "10mb" }));
-// app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // Health check endpoint
 app.get("/api/health", (req, res) => {
